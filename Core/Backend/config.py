351e5541acbd4acfc1f917912020cb12338faf9a
ThibNach <thibaut.spreux@gmail.com>
2026-04-09 09:15:38 +0200

    setup config

---
+++ Core/Backend/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from dataclasses import dataclass
+import os
+
+load_dotenv()
+
+@dataclass
+class Config:
+    DB_NAME :str = os.getenv("DB_NAME")
+    DB_HOST :str = os.getenv("DB_HOST")
+    DB_PORT :str = os.getenv("DB_PORT")
+    DB_USER :str = os.getenv("DB_USER")
+    DB_PASSWORD :str = os.getenv("DB_PASSWORD")
+
+config = Config()
