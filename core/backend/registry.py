f76f60bd5138976d78fc75b2bd290c14649f3fa6
ThibNach <thibaut.spreux@gmail.com>
2026-04-14 11:09:47 +0200

    Improve module registering error handling

--- core/backend/registry.py
+++ core/backend/registry.py
@@ -28,17 +28,17 @@ def register_addons(app):
         try:
             with open( module / MODULEFILE) as file: #with statement handle the file close
                 manifest = json.load(file)
-                if manifest["db_schema_path"]:
+                if manifest.get("db_schema_path"):
                     create_tables_if_not_exist(module / manifest["db_schema_path"], module.parts[-1])
                 
                 loaded_addons.append(file)
+
+                parts = Path(module / BACKENDDIR).parts
+                module_name = '.'.join(parts[parts.index(ADDONSDIR):])
+                importlib.import_module(module_name).setup(app)
         except FileNotFoundError:
-            raise f"File not found : {module / MODULEFILE}"
+            raise FileNotFoundError(f"File not found : {module / MODULEFILE}")
         except json.JSONDecodeError as e:
             raise ValueError(f"invalid Json file : {e}")
                     
-        parts = Path(module / BACKENDDIR).parts
-        module_name = '.'.join(parts[parts.index(ADDONSDIR):])
-        importlib.import_module(module_name).setup(app)
-
     return loaded_addons
