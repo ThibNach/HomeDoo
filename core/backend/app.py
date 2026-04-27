11d7e0dff2a99d2f6b2db39f4fd1c4f67722280c
ThibNach <thibaut.spreux@gmail.com>
2026-04-27 13:48:45 +0200

    [CORE] quick auto code review

--- core/backend/app.py
+++ core/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify,send_from_directory, request
+from flask import Flask, jsonify,send_from_directory
 from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
 from pathlib import Path
