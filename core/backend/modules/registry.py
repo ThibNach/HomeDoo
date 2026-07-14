0b4970493bc30c7873bdfe0b9c6cceb60103c9d3
Thibaut <thibaut.spreux@gmail.com>
2026-07-14 14:40:11 +0200

    [IMP] Adapted registru to use new classes

--- core/backend/modules/registry.py
+++ core/backend/modules/registry.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 
 from database import database
+from .manifest import Manifest
 from .repository import modules_repository
 from utils import singleton
 from .utils import load_manifest
@@ -20,23 +21,22 @@ sys.path.insert(0, str(project_root))
 @singleton
 class Registry:
 
-    def fetch_addons(self, path):
-
+    def fetch_addons(self, path) -> dict[str,Manifest]:
         modules = {}
         for directory in Path.iterdir(project_root / path):
             if directory.is_dir():
-                manifest = directory / MODULE_FILE
-                if manifest.exists():
+                manifest_file = directory / MODULE_FILE
+                if manifest_file.exists():
                      loaded_manifest = load_manifest(directory)
-                     loaded_manifest["path"] = directory
-                     modules[loaded_manifest["name"].lower()] = loaded_manifest
+                     manifest = Manifest.from_json(loaded_manifest, directory)
+                     modules[manifest.name.lower()] = manifest
     
         return modules
 
-    def sort_by_dependencies(self, manifests):
+    def sort_by_dependencies(self, manifests : dict[str, Manifest]) -> list[str]:
         dependencies = {
-            v["name"].lower(): [d.lower() for d in v.get("dependencies", [])]
-            for v in manifests.values()
+            manifest.name.lower(): [dependency.lower() for dependency in manifest.dependencies]
+            for manifest in manifests.values()
         }
         degrees = {name: len(dep) for name, dep in dependencies.items()}
         queue = [name for name, count in degrees.items() if count == 0]
@@ -57,22 +57,25 @@ class Registry:
         return order_list
 
 
-    def register_addons(self, app):
+    def register_addons(self, app) -> list[Manifest]:
         loaded_addons = []
         gathered_addons = self.fetch_addons(project_root / CORE_ADDONS_DIR)
         gathered_addons.update(self.fetch_addons(project_root / ADDONS_DIR))
         sorted_addons = self.sort_by_dependencies(gathered_addons)
-        installed_addons_names = [module.get("name").lower() for module in modules_repository.get_all_installed()]
+        installed_addons_names = [module.name.lower() for module in modules_repository.get_all_installed()]
+        
         for addon in sorted_addons:
             manifest = gathered_addons.get(addon)
-            if addon not in installed_addons_names and not manifest.get("core_module"):
+            
+            if addon not in installed_addons_names and not manifest.core_module:
                 continue
-            if manifest.get("db_schema_path"):
-                database.create_tables_if_not_exist(manifest["path"] / manifest["db_schema_path"], manifest["name"])
+                
+            if manifest.db_schema_path:
+                database.create_tables_if_not_exist(manifest.path / manifest.db_schema_path, manifest.name)
     
             loaded_addons.append(manifest)
-            parts = Path(manifest.get("path") / BACKEND_DIR).parts
-            start = parts.index(CORE_ADDONS_DIR.split('/')[0]) if manifest.get("core_module") else parts.index(ADDONS_DIR)
+            parts = Path(manifest.path / BACKEND_DIR).parts
+            start = parts.index(CORE_ADDONS_DIR.split('/')[0]) if manifest.core_module else parts.index(ADDONS_DIR)
             module_name = '.'.join(parts[start:])
             importlib.import_module(module_name).setup(app)
     
